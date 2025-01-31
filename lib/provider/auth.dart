dfbab3d96b1b87de14e0646234f860ee209005d4
Joyy-dev <joyceava874@gmail.com>
2025-01-31 11:18:58 -0800

    proxy provider

--- lib/provider/auth.dart
+++ lib/provider/auth.dart
@@ -4,9 +4,18 @@ import 'package:http/http.dart' as http;
 import 'package:shop_project/model/http_exception.dart';
 
 class Auth with ChangeNotifier {
-  String _token = '';
-  String _expiringDate = '';
-  String _userId = '';
+  String? _token;
+  DateTime? _expiringDate;
+  String? _userId;
+
+  bool get isAuth {}
+
+  String? get token {
+    if (_expiringDate != null && _expiringDate!.isAfter(DateTime.now()) && _token != null) {
+      return _token;
+    }
+    return null;
+  }
 
   Future<void> _authenticate(String email, String password, String urlSegment) async {
     final url = 'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyA20dW7ttKijTIlaj12MZWnX8N4deLykss';
@@ -24,7 +33,7 @@ class Auth with ChangeNotifier {
       throw HttpException(responseData['error']['message']);
     }
     _token = responseData['idToken'];
-    _expiringDate = DateTime.now().add(Duration(seconds: int.parse(responseData['expiresIn']))).toString();
+    _expiringDate = DateTime.now().add(Duration(seconds: int.parse(responseData['expiresIn'])));
     _userId = responseData['localId'];
     } catch (error) {
       rethrow;
@@ -38,17 +47,5 @@ class Auth with ChangeNotifier {
 
   Future<void> login(String email, String password) async {
     return _authenticate(email, password, 'signInWithPassword');
-  }
-
-  String get token {
-    return _token;
-  }
-
-  String get userId {
-    return _userId;
-  }
-
-  String get expiringDate {
-    return _expiringDate;
   }
 }
